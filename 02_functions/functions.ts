5b8dc08986e94ba71aa97af74d27d1edb2c09200
Leonardo Ramos dos Santos <leonardo.ramosantos@yahoo.com.br>
2024-04-07 14:46:03 -0300

    Functions

--- 02_functions/functions.ts
+++ 02_functions/functions.ts
@@ -2,6 +2,10 @@ function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
+function multiply(n1: number, n2: number) {
+    return n1 * n2;
+}
+
 function showResult(result: string) {
     console.log("The result is: " + result);
 }
@@ -14,3 +18,45 @@ function showResultUndefined(result: string): undefined {
     // To use undefined, you have to return
     return;
 }
+
+type mathFunction = (a: number, b: number) => number;
+
+function addAndMultiply(functions: mathFunction[]) {
+    for (const funct of functions) {
+        console.log(funct.name + " - " + funct(10, 15));
+    }
+}
+addAndMultiply([add, multiply]);
+
+function calculateAndShowResult(functions: mathFunction[], n1: number, n2: number, callback: (a: string) => void) {
+    for (const funct of functions) {
+        callback(funct(n1, n2).toString());
+    }
+}
+
+calculateAndShowResult([add, multiply], 5, 7, showResultUndefined);
+
+//------
+// UNKNOWN
+
+let name: unknown;
+let nameString: string;
+
+name = 10;
+name = "Maria"
+// It will not work. You have to check the type first
+// nameString = name;
+if (typeof name === "string") {
+    nameString = name;
+}
+
+// ------
+// NEVER
+
+function errorGenerator(message: string, code: number): never {
+    throw { message: message, errorCode: code }
+}
+
+const result = errorGenerator("ERROR MESSAGE", 500);
+// won't execute if the function is not declared as returning Never
+console.log("Works");
--- 02_functions/out/functions.js
+++ 02_functions/out/functions.js
@@ -1,6 +1,9 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+function multiply(n1, n2) {
+    return n1 * n2;
+}
 function showResult(result) {
     console.log("The result is: " + result);
 }
@@ -10,3 +13,17 @@ function showResultUndefined(result) {
     // To use undefined, you have to return
     return;
 }
+function addAndMultiply(functions) {
+    for (var _i = 0, functions_1 = functions; _i < functions_1.length; _i++) {
+        var funct = functions_1[_i];
+        console.log(funct.name + " - " + funct(10, 15));
+    }
+}
+addAndMultiply([add, multiply]);
+function calculateAndShowResult(functions, n1, n2, callback) {
+    for (var _i = 0, functions_2 = functions; _i < functions_2.length; _i++) {
+        var funct = functions_2[_i];
+        callback(funct(n1, n2).toString());
+    }
+}
+calculateAndShowResult([add, multiply], 5, 7, showResultUndefined);
